be1518c2f6205fad78cd716974c089357dea8d1a
yuta-kusuno <yuta.ksp@gmail.com>
2020-03-29 20:36:32 +0900

    replace tokushima

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ const config: Configuration = {
     htmlAttrs: {
       prefix: 'og: http://ogp.me/ns#'
     },
-    titleTemplate: '%s | 香川県 新型コロナウイルス感染症対策サイト',
+    titleTemplate: '%s | 徳島県 新型コロナウイルス感染症対策サイト',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -35,7 +35,7 @@ const config: Configuration = {
       }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/Tokushima.svg' },
       { rel: 'apple-touch-icon', href: '/apple-touch-icon-precomposed.png' },
       {
         rel: 'stylesheet',
@@ -130,7 +130,7 @@ const config: Configuration = {
     hardSource: process.env.NODE_ENV === 'development'
   },
   manifest: {
-    name: '香川県 新型コロナウイルス感染症対策サイト',
+    name: '徳島県 新型コロナウイルス感染症対策サイト',
     theme_color: '#00a040',
     background_color: '#ffffff',
     display: 'standalone',
