9391e15e53c4d9fa86896f35851205799d21a51d
yuta-kusuno <yuta-kusuno@users.noreply.github.com>
2020-12-01 12:17:00 +0000

    deploy: c720997f60a46a85453f196424cd4d6d7211d150

---
+++ _nuxt/2ba48057fab6cd562be2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{455:function(t,e,n){"use strict";n.r(e);var r=n(7),component=Object(r.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("a",{attrs:{href:"https://www.pref.tokushima.lg.jp/"}},[this._v("このサイトは終了しました。最新の情報は徳島県の公式HPでご確認ください。")])}),[],!1,null,null,null);e.default=component.exports}}]);
